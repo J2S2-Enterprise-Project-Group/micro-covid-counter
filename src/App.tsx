893981305d6f1092595b1fbfb38587463e3f146d
shannonphu <shannonphu@gmail.com>
2020-11-15 16:47:16 -0800

    add google signin to react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './micro-covid-logo.png';
 import './App.css';
 import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 import Nav from './components/Nav/Nav';
 import CreateActivityForm from './components/CreateActivityForm/CreateActivityForm';
+import { Auth } from 'aws-amplify';
+import { CognitoUserInterface } from '@aws-amplify/ui-components';
 
 interface IAppContainer {
 }
 
 export const App: React.FC<IAppContainer> = (): JSX.Element => {
+  const [user, setUser] = useState<CognitoUserInterface | undefined>(undefined);
+
+  useEffect(() => {
+    getUser().then(userData => {
+      setUser(userData)
+    });
+  }, []);
+
+  function getUser() {
+    return Auth.currentAuthenticatedUser()
+      .then(userData => userData)
+      .catch(() => console.log('Not signed in'));
+  }
+
   return (
     <div className="App">
       <Nav />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>
-          Micro Covid Counter
-        </h3>
-      </header>
-      <Container maxWidth="sm">
-        <CreateActivityForm />
+      <Container maxWidth="lg">
+        {user ? (
+          <div>
+            <Typography variant="h3" gutterBottom>Welcome, {user.attributes.name}</Typography>
+            <CreateActivityForm />
+          </div>
+        ) : (
+          <div>
+            <Typography variant="h3" gutterBottom>Please sign in to microCovid</Typography>
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+            </header>
+          </div>
+        )}
       </Container>
     </div>
   );
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,36 +1,74 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import React, { useEffect, useState } from 'react';
+
+import { Auth, Hub } from 'aws-amplify';
+import { CognitoHostedUIIdentityProvider } from "@aws-amplify/auth/lib/types";
+
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import AccountCircle from '@material-ui/icons/AccountCircle';
 import CreateIcon from '@material-ui/icons/Create';
 
-function Nav() {
+interface INav {
+}
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    title: {
+      flexGrow: 1,
+    }
+  })
+);
+
+export const Nav: React.FC<INav> = (): JSX.Element => {
+  const [user, setUser] = useState(null);
+  const classes = useStyles();
+
+  useEffect(() => {
+    Hub.listen('auth', ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+        case 'cognitoHostedUI':
+          getUser().then(userData => setUser(userData));
+          break;
+        case 'signOut':
+          setUser(null);
+          break;
+        case 'signIn_failure':
+        case 'cognitoHostedUI_failure':
+          console.log('Sign in failure', data);
+          break;
+      }
+    });
+
+    getUser().then(userData => setUser(userData));
+  }, []);
+
+  function getUser() {
+    return Auth.currentAuthenticatedUser()
+      .then(userData => userData)
+      .catch(() => console.log('Not signed in'));
+  }
+
   return (
     <div className="Nav">
       <AppBar position="static">
         <Toolbar>
-          <Grid
-            justify="space-between"
-            container
-            spacing={3}
-          >
-            <Grid item>
-              <Typography variant="h6" color="inherit">
-                microCovid
+          <Typography variant="h6" color="inherit" className={classes.title}>
+            microCovid
               </Typography>
-            </Grid>
-            <Grid item>
+          {user ? (
+            <div>
               <IconButton aria-haspopup="true" color="inherit">
                 <CreateIcon />
               </IconButton>
-              <IconButton aria-haspopup="true" color="inherit">
-                <AccountCircle />
-              </IconButton>
-            </Grid>
-          </Grid>
+              <Button aria-haspopup="true" color="inherit" onClick={() => Auth.signOut()}>Log out</Button>
+            </div>
+          ) : (
+              <Button aria-haspopup="true" color="inherit" onClick={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google })}>Sign in</Button>
+            )}
         </Toolbar>
       </AppBar>
     </div>
