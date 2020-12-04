10424353da8a584f3528ddccccfed5f260cc950f
shannonphu <shannonphu@gmail.com>
2020-12-03 22:31:54 -0800

    Fix group error

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import UserDashboard from './components/UserDashboard/UserDashboard';
 import { Auth } from 'aws-amplify';
 import { CognitoUserInterface } from '@aws-amplify/ui-components';
 import { Route, BrowserRouter, Switch } from 'react-router-dom'
-import CreateGroupForm from './components/CreateGroupForm/CreateGroupForm';
 interface IAppContainer {
 }
 
@@ -39,7 +38,6 @@ export const App: React.FC<IAppContainer> = (): JSX.Element => {
               <Route exact path="/" component={() => <Landing isLoggedIn={user !== undefined} />} />
               {user && <Route exact path="/activity/new" component={ActivityLogger} />}
               {user && <Route exact path="/dashboard" component={UserDashboard} />}
-              {user && <Route exact path="/groups/create" component={CreateGroupForm} />}
               {user && <Route exact path="/people" component={UserList} />}
             </Switch>
           </Container>
